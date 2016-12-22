9b4833c8815a0f905c044f18234c61529e1464c6
Jose Lezama Gonzalez <joseglego@gmail.com>
2016-12-22 16:23:34 -0400

    Second session of 1hour1app and includes:
    - Filter by category
    - Filter by Menu or Current Order
    - Added icons
    - Improces on visual
    - Added real prices

--- scripts/app.js
+++ scripts/app.js
@@ -5,29 +5,53 @@ app.controller('myController', [
   'lodash',
   function ($scope, lodash) {
     // Data
+    $scope.categories = [{
+      name: 'hamburgers', icon: 'flaticon-hamburger'
+    },{
+      name: 'subs', icon: 'flaticon-sub'
+    },{
+      name: 'drinks', icon: 'flaticon-drink'
+    },{
+      name: 'extras', icon: 'flaticon-fries'
+    },{
+      name: 'desserts', icon: 'flaticon-dessert'
+    }];
+
     $scope.items = [{
-      name: 'Item 0',
-      price: 10,
-      amount: 0
+      name: 'Hamburguesa Normal', price: 3600, category: 'hamburgers', amount: 0
     }, {
-      name: 'Item 1',
-      price: 15,
-      amount: 0
+      name: 'Hamburguesa Mixta (2 sabores)', price: 4200, category: 'hamburgers', amount: 0
     }, {
-      name: 'Item 2',
-      price: 30,
-      amount: 0
+      name: 'Hamburguesa Mixta (3 sabores)', price: 5000, category: 'hamburgers', amount: 0
     },{
-      name: 'Item 4',
-      price: 20,
-      amount: 0
+      name: 'Hamburguesa Full ', price: 6000, category: 'hamburgers', amount: 0
     },{
-      name: 'Item 5',
-      price: 12,
-      amount: 0
+      name: 'Hamburguesa Sazon y Amor ', price: 4500, category: 'hamburgers', amount: 0
+    },{
+      name: 'Pepito de Carne', price: 7500, category: 'subs', amount: 0
+    },{
+      name: 'Pepito de Pollo', price: 7500, category: 'subs', amount: 0
+    },{
+      name: 'Pepito Mixto', price: 9500, category: 'subs', amount: 0
+    },{
+      name: 'Pepito Full', price: 12500, category: 'subs', amount: 0
+    },{
+      name: 'Ración de Papas Fritas', price: 2500, category: 'extras', amount: 0
+    },{
+      name: 'Malta', price: 700, category: 'drinks', amount: 0
+    },{
+      name: 'Refresco de Litro y Medio', price: 3200, category: 'drinks', amount: 0
+    },{
+      name: 'Nestea por Vaso', price: 700, category: 'drinks', amount: 0
+    },{
+      name: 'Nestea por Jarra', price: 2700, category: 'drinks', amount: 0
+    },{
+      name: 'Postres', price: 1500, category: 'desserts', amount: 0
     }];
 
     $scope.total = 0;
+    $scope.selectedCategory = '';
+    $scope.amount = -1;
 
     // Functions
     $scope.set = function (item, amount) {
@@ -45,5 +69,11 @@ app.controller('myController', [
         $scope.set(item,0);
       });
     }
+
+    $scope.greatherThan = function (number) {
+      return function (item) {
+        return item.amount > number;
+      }
+    };
   }
 ])
